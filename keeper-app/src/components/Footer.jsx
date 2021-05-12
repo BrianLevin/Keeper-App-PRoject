ba2b5a822231abf368ef8f82fb687013d95e3011
BrianLevin <bml201095@gmail.com>
2021-05-12 10:53:34 -0400

    exported the footer app to the app.js file

---
+++ keeper-app/src/components/Footer.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+
+function Footer(){
+
+
+    const currentYear = new Date().getFullYear();
+
+
+
+    return <p>
+Copyright © {currentYear}
+
+    </p>
+
+
+    
+}
+
+
+export default Footer;
