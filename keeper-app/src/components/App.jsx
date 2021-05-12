8eb1e0bb61d032a4ed5f413a126363c4c5353fb0
BrianLevin <bml201095@gmail.com>
2021-05-12 10:43:40 -0400

    imported the header component to the app file

--- keeper-app/src/components/App.jsx
+++ keeper-app/src/components/App.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 
+import Header from "./Header"
+
 function App(){
 
-    return <div>
-<h1>Hello App </h1>
+    return  (<div>
+<Header />
 
     </div>
+    );
 }
 
 
