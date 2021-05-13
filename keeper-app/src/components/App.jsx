a8957015e2a5768f4605311ff5541450770c605b
BrianLevin <bml201095@gmail.com>
2021-05-13 12:22:59 -0400

    moved the creates function into the map function to be defined by another function and at the position it is being used, noting else has changed

--- keeper-app/src/components/App.jsx
+++ keeper-app/src/components/App.jsx
@@ -11,22 +11,23 @@ import notes from "../notes"
 
 
 function App(){
-// function which renders creates notes and renders a single note component
+
+    return  (
+        <div>
+<Header />
+{/* map which renders through and displays all the notes into the keeper app as seperate note componeents */}
+{notes.map(// function which renders creates notes and renders a single note component
     function createNotes(noteItem){
 return <Note 
-
-// props to pass to the note.js file
+// key makes prop unique so method can utilze dynamic arrays
+key = {noteItem.key}
+// prop values to pass to the note.js file
 title= {noteItem.title}
 content= {noteItem.content}
 
 />
 
-    }
-    return  (
-        <div>
-<Header />
-{/* map which renders through and displays all the notes */}
-{notes.map(createNotes)}
+    })}
 <Footer />
 
 </div>
