8444c78e6f0471f607c1f76a687f80b21e569794
BrianLevin <bml201095@gmail.com>
2021-05-16 15:06:12 -0400

    created a function which will update the changes

--- keeper-app/src/components/CreateArea.jsx
+++ keeper-app/src/components/CreateArea.jsx
@@ -8,13 +8,20 @@ content: ""
 
 
 }) 
+// function which will update the changes
+function handleChange(event){
+
+  // create new destructured object which will tap into event .taget.name and event.target.value
+  const{name,value}= event.target;
+
+}
 
 function CreateArea() {
   return (
     <div>
       <form>
-        <input name="title"  value= {note.title} placeholder="Title" />
-        <textarea name="content"  value = {note.content} placeholder="Take a note..." rows="3" />
+        <input name="title"  onChange= {handleChange} value= {note.title} placeholder="Title" />
+        <textarea name="content"  onChange = {handleChange} value = {note.content} placeholder="Take a note..." rows="3" />
         <button>Add</button>
       </form>
     </div>
