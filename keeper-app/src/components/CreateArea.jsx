b58d82f4def792a5bc07a12b2912ade492a7a4ad
BrianLevin <bml201095@gmail.com>
2021-05-16 14:59:42 -0400

    set the state for the note and set note so the user can change the title and content of what they are targeting  on the form

--- keeper-app/src/components/CreateArea.jsx
+++ keeper-app/src/components/CreateArea.jsx
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+//stateful constants with object containing title and content
+// const note function setNote
+const [note,setNote] = useState({
+    // intial state is empty
+title: "",
+content: ""
+
+
+}) 
 
 function CreateArea() {
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
+        <input name="title"  value= {note.title} placeholder="Title" />
+        <textarea name="content"  value = {note.content} placeholder="Take a note..." rows="3" />
         <button>Add</button>
       </form>
     </div>
